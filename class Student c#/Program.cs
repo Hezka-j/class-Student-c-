bb6b1314e3f417757ba6ce744922f58ce539c3e3
Hezka-j <145163687+Hezka-j@users.noreply.github.com>
2024-06-26 16:38:45 +0300

    Add files via upload

---
+++ class Student c#/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace class_Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Print();
+        }
+    }
+}
